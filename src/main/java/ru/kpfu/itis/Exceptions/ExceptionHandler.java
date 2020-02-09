5493a66c7b1ff492af1e4a6fdc3dbecd12429723
RuefulMage <feonorabd2000@gmail.com>
2020-02-09 17:27:04 +0300

    Exception handler added

---
+++ src/main/java/ru/kpfu/itis/Exceptions/ExceptionHandler.java
@@ -0,0 +1,21 @@
+package ru.kpfu.itis.Exceptions;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Slf4j
+@ControllerAdvice
+public class ExceptionHandler {
+
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ResponseBody
+    public ErrorResponse handleIllegalArgument(final IllegalArgumentException e) {
+        log.info(e.getMessage());
+        return new ErrorResponse("NOT_FOUND", e.getMessage());
+    }
+}
