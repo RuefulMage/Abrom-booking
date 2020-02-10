486378b3d5885966f06a8acf078019f3dc3f4dbf
RuefulMage <feonorabd2000@gmail.com>
2020-02-10 12:59:08 +0300

    Some bugs fixed

--- src/main/java/ru/kpfu/itis/Controllers/LoginController.java
+++ src/main/java/ru/kpfu/itis/Controllers/LoginController.java
@@ -32,16 +32,23 @@ public class LoginController {
         this.userService = userService;
     }
 
+//    @PostMapping("/login")
+//    public ResponseEntity<Map> login(@RequestBody LoginForm loginForm){
+//        TokenDTO tokenDTO = loginService.login(loginForm);
+//        Role role = userService.findOneByLogin(loginForm.getLogin()).getRole();
+//        Map map = new HashMap();
+//        map.put("value", tokenDTO.getToken());
+//        if(role.name().equals(Role.ROLE_ADMIN)) {
+//            map.put("role", role);
+//        }
+//        return ResponseEntity.ok(map);
+//    }
+
     @PostMapping("/login")
-    public ResponseEntity<Map> login(@RequestBody LoginForm loginForm){
+    public ResponseEntity<TokenDTO> login(@RequestBody LoginForm loginForm){
         TokenDTO tokenDTO = loginService.login(loginForm);
         Role role = userService.findOneByLogin(loginForm.getLogin()).getRole();
-        Map map = new HashMap();
-        map.put("value", tokenDTO.getToken());
-        if(role.name().equals(Role.ROLE_ADMIN)) {
-            map.put("role", role);
-        }
-        return ResponseEntity.ok(map);
+        return ResponseEntity.ok(tokenDTO);
     }
 
     @PostMapping("/logout")
